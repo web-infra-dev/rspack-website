9c861a1d2d813e3d5a4fa3be20e76d9b680ec677
chenjiahan <chenjiahan.jait@bytedance.com>
2023-02-24 00:17:18 +0800

    chore: add tailwindcss support

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './src/**/*.{html,js,jsx,tx,tsx}',
+    './components/**/*.{js,jsx,tx,tsx}',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
