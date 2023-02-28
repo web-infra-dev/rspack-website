336151c1ebc4d7256ef12ba3360e784acb4290bb
hardfist <yangjian.fe@bytedance.com>
2023-02-28 13:54:01 +0800

    chore: move list

--- components/random-member-list.tsx
+++ components/random-member-list.tsx
@@ -1,7 +1,51 @@
 interface Member {
   id: string;
 }
-export const RandomMemberList = ({ list }: { list: Member[] }) => {
+export const RandomMemberList = () => {
+  const list = [
+    {
+      id: 'zoolsher',
+    },
+    {
+      id: 'hardfist',
+    },
+    {
+      id: 'ahabhgk',
+    },
+    {
+      id: 'bvanjoi',
+    },
+    {
+      id: 'Boshen',
+    },
+    {
+      id: 'h-a-n-a',
+    },
+    {
+      id: 'hyf0',
+    },
+    {
+      id: 'IWANABETHATGUY',
+    },
+    {
+      id: 'jerrykingxyz',
+    },
+    {
+      id: 'underfin',
+    },
+    {
+      id: 'neverland',
+    },
+    {
+      id: 'JSerFeng',
+    },
+    {
+      id: '9aoy',
+    },
+    {
+      id: 'sanyuan0704',
+    },
+  ];
   const randomList = list.sort(() => Math.random() - 0.5);
   return (
     <ul>
