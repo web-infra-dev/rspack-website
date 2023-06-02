3028d392f33f51552e53d9f565798a7491db983d
Hana <andywangsy@gmail.com>
2023-06-02 12:51:43 +0800

    docs: release blog 0.2 (#342)
    * docs: init release blog
    
    * docs: update translation
    
    * chore: update links
    
    * docs: update Modern.js part in 0.2 blog
    
    * fix: update module format doc
    
    * Update docs/zh/blog/announcing-0.2.md
    
    Co-authored-by: neverland <chenjiahan.jait@bytedance.com>
    
    * Apply suggestions from code review
    
    Co-authored-by: Gengkun <ahabhgk@gmail.com>
    
    * docs: update wording
    
    * docs: update vue
    
    ---------
    
    Co-authored-by: chenjiahan <chenjiahan.jait@bytedance.com>
    Co-authored-by: underfin <likui6666666@gmail.com>
    Co-authored-by: Gengkun <ahabhgk@gmail.com>

--- modern.config.ts
+++ modern.config.ts
@@ -31,7 +31,7 @@ function getNavConfig(lang: 'zh' | 'en'): NavItem[] {
     },
     {
       text: getText('博客', 'Blog'),
-      link: getLink('/blog/announcement'),
+      link: getLink('/blog/announcing-0.2'),
       activeMatch: '/blog',
     },
     {
@@ -293,6 +293,10 @@ function getSidebarConfig(lang: 'zh' | 'en'): Sidebar {
       },
     ],
     [getLink('/blog/')]: [
+      {
+        text: getText('0.2 发布公告', 'Announcing Rspack 0.2'),
+        link: getLink('/blog/announcing-0.2'),
+      },
       {
         text: getText('发布公告', 'Announcing Rspack'),
         link: getLink('/blog/announcement'),
