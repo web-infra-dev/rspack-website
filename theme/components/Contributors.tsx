7600b9cfdd399d12c95cca18a911be83605b3da3
bekzodnj <bekzodnx@gmail.com>
2023-06-03 01:59:59 +0200

    styles: Contributions block fix

--- theme/components/Contributors.tsx
+++ theme/components/Contributors.tsx
@@ -5,10 +5,7 @@ export const Contributors: FC = () => (
     <hr />
     <div className="flex flex-col my-4 items-center overflow-x-scroll">
       <h2 className="text-2xl mb-4">Contributors</h2>
-      <object
-        className="pl-[540px]"
-        data="https://opencollective.com/rspack/contributors.svg?width=900&button=false"
-      />
+      <object data="https://opencollective.com/rspack/contributors.svg?width=900&button=false" />
     </div>
   </>
 );
