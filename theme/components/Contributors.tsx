7a06a831caf7800dbb5eb5f8ad0dbcc8d887c25e
Bekzodjon Norkuziev <34368503+bekzodnj@users.noreply.github.com>
2023-06-07 08:46:55 +0200

    style: mobile landing fix, contribution list fix (#353)
    * style: mobile landing, contribution list fixes
    
    * styles: Contributions block fix

--- theme/components/Contributors.tsx
+++ theme/components/Contributors.tsx
@@ -3,8 +3,8 @@ import { FC } from 'react';
 export const Contributors: FC = () => (
   <>
     <hr />
-    <div className="flex flex-col my-4 items-center">
-      <div className="text-2xl mb-4">Contributors</div>
+    <div className="flex flex-col my-4 items-center overflow-x-scroll">
+      <h2 className="text-2xl mb-4">Contributors</h2>
       <object data="https://opencollective.com/rspack/contributors.svg?width=900&button=false" />
     </div>
   </>
