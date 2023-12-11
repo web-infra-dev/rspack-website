da18f64798a1a2f837bb8eda5d8358b98792ab9a
hardfist <yangjianzju@gmail.com>
2023-12-11 22:30:20 +0800

    chore: move builtin:swc-loader into separate page (#556)

--- rspress.config.ts
+++ rspress.config.ts
@@ -143,6 +143,7 @@ function getSidebarConfig(lang: 'zh' | 'en'): Sidebar {
             text: getText('Web Workers', 'Web Workers'),
           },
           getLink('/guide/loader'),
+          getLink('/guide/builtin:swc-loader'),
           {
             link: getLink('/guide/plugin'),
             text: getText('Plugin', 'Plugin'),
