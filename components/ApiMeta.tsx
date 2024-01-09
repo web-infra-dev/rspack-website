5f0615fb924872b14a6b6808e021e65998c01525
ahabhgk <ahabhgk@gmail.com>
2024-01-09 20:04:19 +0800

    update oudated doc

--- components/ApiMeta.tsx
+++ components/ApiMeta.tsx
@@ -1,11 +1,12 @@
 import { useLang } from 'rspress/runtime';
 import './ApiMeta.scss';
+
 /**
  * The Stability Index is learned from https://nodejs.org/api/documentation.html#stability-index
-
  */
 export enum Stability {
   Deprecated = 'Deprecated', // The feature may emit warnings. Backward compatibility is not guaranteed.
+  Removed = 'Removed',
   Experimental = 'Experimental', // The feature is not subject to semantic versioning rules
   Stable = 'Stable', // Compatibility with the npm ecosystem is a high priority.
   Legacy = 'Legacy', // Although this feature is unlikely to be removed and is still covered by semantic versioning guarantees, it is no longer actively maintained, and other alternatives are available.
@@ -14,6 +15,7 @@ export enum Stability {
 export interface ApiMetaProps {
   addedVersion?: string;
   deprecatedVersion?: string;
+  removedVersion?: string;
   stability?: Stability;
 }
 
@@ -21,7 +23,9 @@ export function ApiMeta(props: ApiMetaProps) {
   let lang = useLang();
   return (
     <div className="api-meta">
-      {(!!props.addedVersion || !!props.deprecatedVersion) && (
+      {(!!props.addedVersion ||
+        !!props.deprecatedVersion ||
+        !!props.removedVersion) && (
         <div className="api-meta-version">
           {!!props.addedVersion && (
             <span className="api-meta-version-added">
@@ -34,6 +38,15 @@ export function ApiMeta(props: ApiMetaProps) {
                 href={`/${lang}/misc/future?deprecatedVersion=${props.deprecatedVersion}`}
               >
                 v{props.deprecatedVersion}
+              </a>
+            </span>
+          )}
+          {!!props.removedVersion && (
+            <span className="api-meta-version-removed">
+              <a
+                href={`/${lang}/misc/future?removedVersion=${props.removedVersion}`}
+              >
+                v{props.removedVersion}
               </a>
             </span>
           )}
